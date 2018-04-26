b41c6433121a7e2318a6e2abfcddca55e7703b07
Derek Shoemaker <shoemakerdr@gmail.com>
2018-04-26 12:56:16 -0500

    Moved limiter and countNeighbors functions to their own scope. Finished implementation of nextGeneration.

--- src/Board.hs
+++ src/Board.hs
@@ -1,7 +1,5 @@
 module Board where
 
-
-
 import Data.Matrix ( Matrix, getElem, submatrix
                    , toList, toLists, nrows, ncols
                    , fromList
@@ -15,54 +13,30 @@ data Cell
     | Alive
         deriving (Show, Eq)
 
-
 type Board =
     Matrix Cell
-
 
 type Position = (Int, Int)
 
 
-
 nextGeneration :: Board -> Board
 nextGeneration board =
-    traceShow something $ fmap (const Dead) board
-    -- fromList rows cols
-        -- (progressCell board <$> zip [1..rows] [1..cols])
+    -- traceShow something $ fmap (const Dead) board
+    fromList rows cols progressed
     where
         rows = nrows board
         cols = ncols board
-        something = progressCell board <$> [(x,y) | x <- [1..rows], y <- [1..cols]]
+        progressed =
+            progressCell board
+                <$> [(x,y) | x <- [1..rows], y <- [1..cols]]
 
 
 progressCell :: Board -> Position -> Cell
 progressCell board pos =
-    cycleCell (neighbors pos board) cell
+    cycleCell (countNeighbors pos board) cell
         where
-            cell = getElem row col board
-            row = fst pos
-            col = snd pos
-            neighbors pos board =
-                (+) offset $ length $ filter (Alive ==) neighborhood
-                    where
-                        neighborhood =
-                            toList $ 
-                                submatrix 
-                                    (limit $ row - 1)
-                                    (limit $ row + 1)
-                                    (limit $ col - 1)
-                                    (limit $ col + 1) board
-                        offset =
-                            case cell of
-                                Alive -> -1
-                                Dead -> 0
-                        limit = limitInt board
-                        limitInt matrix i 
-                            | i < 1 = 1
-                            | i > upper = upper
-                            | otherwise = i
-                                where
-                                    upper = length $ head $ toLists matrix
+            cell = uncurry getElem pos board
+
 
 cycleCell :: Int -> Cell -> Cell
 cycleCell neighbors Alive
@@ -72,3 +46,36 @@ cycleCell neighbors Alive
 cycleCell neighbors Dead
     | neighbors == 3 = Alive
     | otherwise = Dead
+
+
+limiter :: Matrix a -> (Matrix a -> Int) -> Int -> Int
+limiter matrix withinUpper i
+    | i < 1 = 1
+    | i > upper = upper
+    | otherwise = i
+        where
+            upper = withinUpper matrix
+
+
+countNeighbors :: Position -> Board -> Int
+countNeighbors pos board =
+    (+) offset $ length $ filter (Alive ==) neighborhood
+        where
+            row = fst pos
+            col = snd pos
+            neighborhood =
+                toList $ 
+                    submatrix 
+                        (limitRow $ row - 1)
+                        (limitRow $ row + 1)
+                        (limitCol $ col - 1)
+                        (limitCol $ col + 1) board
+            cell = getElem row col board
+            offset =
+                case cell of
+                    Alive -> -1
+                    Dead -> 0
+            limitRow = limiter board nrows
+            limitCol = limiter board ncols
+
+
--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -1,24 +1,41 @@
 module BoardSpec (main, spec) where
 
-
-
-import Board (Board, Cell(..), nextGeneration, progressCell, cycleCell)
-import Data.Matrix (fromLists)
+import Board (Board, Cell(..), nextGeneration, progressCell, cycleCell, limiter, countNeighbors)
+import Data.Matrix (fromLists, nrows, ncols)
 import Test.Hspec
 
 
 main :: IO ()
 main = hspec spec
 
+
 spec :: Spec
 spec = do
     describe "nextGeneration" $
-        it "kills cells with less than two neighbors" $ do
-            let board = fromLists [ [ Alive, Dead, Dead ]
-                                  , [ Dead, Dead, Dead ]
-                                  , [ Dead, Alive, Dead ]
-                                  ]
-            nextGeneration board `shouldBe` deadBoard
+        it "progresses all cells on the board to the next generation" $ do
+            let board1 = fromLists [ [ Alive, Dead, Dead ]
+                                   , [ Dead, Dead, Dead ]
+                                   , [ Dead, Alive, Dead ]
+                                   ]
+                board2 = fromLists [ [ Alive, Dead, Dead ]
+                                   , [ Alive, Alive, Alive ]
+                                   , [ Dead, Alive, Dead ]
+                                   ]
+                board2Next = fromLists [ [ Alive, Dead, Dead ]
+                                       , [ Alive, Dead, Alive ]
+                                       , [ Alive, Alive, Alive ]
+                                       ]
+                fullBoard = fromLists [ [ Alive, Alive, Alive ]
+                                      , [ Alive, Alive, Alive ]
+                                      , [ Alive, Alive, Alive ]
+                                      ]
+                fullBoardNext = fromLists [ [ Alive, Dead, Alive ]
+                                          , [ Dead, Dead, Dead ]
+                                          , [ Alive, Dead, Alive ]
+                                          ]
+            nextGeneration board1 `shouldBe` deadBoard
+            nextGeneration board2 `shouldBe` board2Next
+            nextGeneration fullBoard `shouldBe` fullBoardNext
 
     describe "progressCell" $
         it "gets next lifecycle of cell" $ do
@@ -26,18 +43,18 @@ spec = do
                                   , [ Alive, Alive, Alive ]
                                   , [ Dead, Alive, Dead ]
                                   ]
-            progressCell board (1, 1) `shouldBe` Alive 
-            progressCell board (1, 2) `shouldBe` Dead 
-            progressCell board (1, 3) `shouldBe` Dead 
-            progressCell board (2, 1) `shouldBe` Alive 
-            progressCell board (2, 2) `shouldBe` Dead 
-            progressCell board (2, 3) `shouldBe` Alive 
+            progressCell board (1, 1) `shouldBe` Alive
+            progressCell board (1, 2) `shouldBe` Dead
+            progressCell board (1, 3) `shouldBe` Dead
+            progressCell board (2, 1) `shouldBe` Alive
+            progressCell board (2, 2) `shouldBe` Dead
+            progressCell board (2, 3) `shouldBe` Alive
             progressCell board (3, 1) `shouldBe` Alive
             progressCell board (3, 2) `shouldBe` Alive
             progressCell board (3, 3) `shouldBe` Alive
 
     describe "cycleCell" $ do
-        describe "an alive cell" $ do
+        describe "with an alive cell" $ do
             it "will die with fewer than two neighbors" $
                 cycleCell 1 Alive `shouldBe` Dead
             it "will stay alive with two or three neighbors" $ do
@@ -49,7 +66,7 @@ spec = do
                 cycleCell 6 Alive `shouldBe` Dead
                 cycleCell 7 Alive `shouldBe` Dead
                 cycleCell 8 Alive `shouldBe` Dead
-        describe "a dead cell" $ do
+        describe "with a dead cell" $ do
             it "will become alive if it has three neighbors" $
                 cycleCell 3 Dead `shouldBe` Alive
             it "stay dead if less than three neighbors" $ do
@@ -62,10 +79,30 @@ spec = do
                 cycleCell 6 Dead `shouldBe` Dead
                 cycleCell 7 Dead `shouldBe` Dead
                 cycleCell 8 Dead `shouldBe` Dead
+    describe "limiter" $
+        it "limits a given int so it is within the bounds of a given matrix" $ do
+            limiter deadBoard nrows 0 `shouldBe` 1
+            limiter deadBoard nrows 4 `shouldBe` 3
+            limiter deadBoard nrows 2 `shouldBe` 2
+
+    describe "countNeighbors" $
+        it "counts the number of living neighbors" $ do
+            let board = fromLists [ [ Alive, Dead, Dead ]
+                                  , [ Alive, Alive, Alive ]
+                                  , [ Dead, Alive, Dead ]
+                                  ]
+            countNeighbors (1,1) board `shouldBe` 2
+            countNeighbors (1,2) board `shouldBe` 4
+            countNeighbors (1,3) board `shouldBe` 2
+            countNeighbors (2,1) board `shouldBe` 3
+            countNeighbors (2,2) board `shouldBe` 4
+            countNeighbors (2,3) board `shouldBe` 2
+            countNeighbors (3,1) board `shouldBe` 3
+            countNeighbors (3,2) board `shouldBe` 3
+            countNeighbors (3,3) board `shouldBe` 3
 
 deadBoard = fromLists [ [ Dead, Dead, Dead ]
                       , [ Dead, Dead, Dead ]
                       , [ Dead, Dead, Dead ]
                       ]
-
 
